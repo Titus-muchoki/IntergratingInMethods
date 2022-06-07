1a1b162d72b6d8c83f1b6a46cabf47ad072f4f80
kajela <titoyut56@gmail.com>
2022-06-07 01:38:47 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,18 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
+    static void myHobbies(String ICT){
+        System.out.println(ICT+"In kca university");
+    }
+    static void homeArea(String mirema){
+        System.out.println(mirema+" along kamiti road");
+    }
+    static void myPionner(String doubleCoil){
+        System.out.println(doubleCoil+" double breather");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        myHobbies("java");
+        homeArea("canopy");
+        myPionner("12D2");
     }
 }
